6e224de24ec1476b604c9ffd252f6438fe02358e
Dominik Gromadzki <dominikgromadzki@op.pl>
2017-03-31 16:27:59 +0200

    addnig gif

--- src/main/java/com/example/controller/GifController.java
+++ src/main/java/com/example/controller/GifController.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
+import com.example.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by dominikgromadzki on 31.03.2017.
@@ -10,8 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GifController {
 
+
+
     @GetMapping("/")
     public String listGifs(){
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot","michalos",true); modelMap.put("gif",gif);
+        return "gif-details";
     }
 }
---
+++ src/main/java/com/example/model/Gif.java
@@ -0,0 +1,45 @@
+package com.example.model;
+
+/**
+ * Created by dominikgromadzki on 31.03.2017.
+ */
+
+    public class Gif {
+        private String name;
+        private String username;
+        private boolean favorite;
+
+    public Gif() {
+    }
+
+    public Gif(String name, String username, boolean favorite) {
+        this.name = name;
+        this.username = username;
+        this.favorite = favorite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+}
+
