13c7b45e6c918b7c91b05b722f30c591b152cc46
Dominik Gromadzki <dominikgromadzki@op.pl>
2017-04-03 10:18:01 +0200

    create all_gifs branch

--- src/main/java/com/example/controller/GifController.java
+++ src/main/java/com/example/controller/GifController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
+import com.example.data.GifRepository;
 import com.example.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by dominikgromadzki on 31.03.2017.
@@ -14,6 +19,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GifController {
 
 
+    @Autowired
+    private GifRepository gifRepository;
+
+    @RequestMapping("/")
+    public String listGifts(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs(); modelMap.put("gifs",allGifs);
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name); modelMap.put("gif",gif);
+        return "gif-details";
+    }
 
     @GetMapping("/")
     public String listGifs(){
---
+++ src/main/java/com/example/data/GifRepository.java
@@ -0,0 +1,30 @@
+package com.example.data;
+
+import com.example.model.Gif;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by dominikgromadzki on 31.03.2017.
+ */
+
+@Component
+public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("android-explosion", "Chris Ramacciotti", false), new Gif("ben-and-mike", "Ben Jakuben", true),
+            new Gif("book-dominos", "Craig Dennis", false),
+            new Gif("compiler-bot", "Ada Lovelace", true),
+            new Gif("cowboy-coder", "Grace Hopper", false),
+            new Gif("infinite-andrew", "Marissa Mayer", true)
+    );
+    public Gif findByName(String name){ for (Gif gif : ALL_GIFS){
+        if ( gif.getName().equals(name)){ return gif;
+        }
+    }
+        return null; }
+
+
+    
+}
